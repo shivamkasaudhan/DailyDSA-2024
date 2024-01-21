db5900da95397f2ec2ee699bcd913a0e46f91fa4
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2024-01-21 23:56:32 +0530

    Day 21

---
+++ 21-Jan/countingBits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int count(int n){
+        int bits=0;
+        while(n>0){
+            bits+=n%2;
+            n/=2;
+        }
+        return bits;
+    }
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0;i<=n;i++){
+            ans.push_back(count(i));
+        }
+        return ans;
+    }
+};
