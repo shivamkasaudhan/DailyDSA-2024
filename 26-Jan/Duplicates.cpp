5a2cc1e9a93e7330b22cf6fe3979f14b2c317cb6
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2024-01-26 23:48:55 +0530

    Day 26

---
+++ 26-Jan/Duplicates.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    vector<int> duplicates(long long arr[], int n) {
+        // code here
+        vector<int> ans;
+        unordered_map<int,int> map;
+        for(int i = 0;i<n;i++){
+            map[arr[i]]++;
+        }
+        for(auto i : map){
+            if(i.second > 1){
+                ans.push_back(i.first);
+            }
+        }
+        if(ans.size() == 0){
+            ans.push_back(-1);
+        }else{
+            sort(ans.begin(),ans.end());
+        }
+        return ans;
+        
+        
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        long long a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
