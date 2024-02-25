39ea8194e3eb0c0ad768116980246900e2b2151f
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2024-02-26 00:06:10 +0530

    Day 56

---
+++ 25-Feb/closestNumber.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int findClosest(int arr[], int n, int target) 
+    { 
+        // Complete the function
+        int index=0;
+        int min=INT_MAX;
+        
+        for(int i=0; i<n; i++){
+          int diff=abs(arr[i]-target);
+          if(min >= diff){
+              min = diff;
+              index = arr[i];
+          }
+        }
+        return index;
+    } 
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,target;
+        cin>>n>>target;
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.findClosest(arr,n,target)<<endl;
+    }
+}
+
+// } Driver Code Ends
