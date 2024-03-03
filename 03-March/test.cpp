0317eec5bd5ceabbd9aae76ace73bacecf512ae3
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2024-03-03 23:12:25 +0530

    Day 62 & 63

---
+++ 03-March/test.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+class find {
+
+public:
+
+void print(){cout<<" In find";}
+
+};
+
+ class course :public find {
+
+public:
+
+void print(){cout<<" In course";}
+
+};
+
+class tech:public course {};
+
+ int main()
+
+{
+
+  tech t;
+
+t.print();
+
+return 0;
+
+}
