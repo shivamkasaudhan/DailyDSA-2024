fe19852f977e15fee7eacda7250379aaab640192
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2024-03-12 23:58:24 +0530

    Day 72

---
+++ 12-March/MinimizeHeight1.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int getMinDiff(int arr[], int n, int k) {
+        // code here
+        if(n==1){
+            return 0;
+        }
+        sort(arr,arr+n);
+        int diff = arr[n-1]-arr[0];
+        int mini,maxi;
+        for(int i=1;i<n;i++){
+            // if(arr[i]-k <0){
+            //     continue;
+            // }
+            mini = min(arr[i]-k, arr[0]+k);
+            maxi =max(arr[n-1]-k,arr[i-1]+k);
+            diff = min(diff,(maxi-mini));
+        }
+        return diff;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> k;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getMinDiff(arr, n, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
